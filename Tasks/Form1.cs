a73714d24eb2d170b43dddf8f603bfee04d0a615
Ahmed Belguith <ahmedbelguith16@gmail.com>
2026-02-19 14:26:06 +0100

    Fixed unclickable Tasks folder

---
+++ Tasks/Form1.cs
@@ -0,0 +1,76 @@
+﻿using businessLayerOfTasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasks
+{
+    // Gemini Test
+    public partial class Form1 : Form
+    {
+        private ITaskServices taskServices;
+        // subscribe to the TaskAdded event to refresh the DataGridView when a new task is added
+        public Form1(ITaskServices service)
+        {
+            InitializeComponent();
+            taskServices = service;
+            // SUBSCRIBE to the event using a Lambda expression
+            taskServices.OnDataChanged += () =>
+            {
+                // This code runs AUTOMATICALLY when AddTask is called
+                RefreshDataGridView();
+                RefreshPrograssBar();
+            };
+        }
+        private void RefreshPrograssBar()
+        {
+                float Progress = taskServices.GetTotalProgress()*100;
+                lblProgress.Text = Progress.ToString()+"%";
+                prgBar.Minimum = 0;
+                prgBar.Maximum = 100;
+                prgBar.Value = (int)(Progress);
+
+        }
+        private void RefreshDataGridView()
+        {
+            if (taskServices.GetAllTasks() != null && taskServices.GetAllTasks().Count!=0)
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource= taskServices.GetAllTasks();
+
+            }
+        }
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            frmAddTask frmAdd=new frmAddTask();
+            frmAdd.ShowDialog();
+            if (frmAdd.DialogResult==DialogResult.OK)
+            {
+                taskServices.AddTask(new clsTask(frmAdd.TaskTitle));
+            }
+
+        }
+
+
+        private void btnCompleteTask_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                if (row.DataBoundItem is clsTask task)
+                {
+                    taskServices.GetTaskDone(task);
+
+                }
+            }
+
+
+        }
+
+    }
+}
